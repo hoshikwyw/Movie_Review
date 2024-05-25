bbd5c0c8cca6733bde1ab70b036f77248796bf87
KhaingWutYiWin <113814930+hoshikwyw@users.noreply.github.com>
2024-05-26 01:31:47 +0630

    Project build

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      this project will be show movies and series rating and reviews and trailers .... 
+    </div>
+  )
+}
+
+export default App
